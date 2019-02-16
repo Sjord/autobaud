f6f5b75073d8efefbb21d59babf8a0a6eda672b5
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2019-02-16 12:01:45 +0100

    Trigger on falling and then rising edge

--- autobaud.ino
+++ autobaud.ino
@@ -1,7 +1,11 @@
+// Autobaud: measure baud rate by timing pulses
+// Sjoerd Langkemper
+// Adapted from:
 
 // Frequency timer using input capture unit
 // Author: Nick Gammon
 // Date: 31 August 2013
+// Licensed under the Creative Commons Attribution 3.0 Australia License
 
 // Input: Pin D8 
 
@@ -12,13 +16,11 @@ volatile unsigned long startTime;
 volatile unsigned long finishTime;
 
 // timer overflows (every 65536 counts)
-ISR (TIMER1_OVF_vect) 
-{
+ISR (TIMER1_OVF_vect)  {
   overflowCount++;
 }  // end of TIMER1_OVF_vect
 
-ISR (TIMER1_CAPT_vect)
-  {
+ISR (TIMER1_CAPT_vect) {
   // grab counter value before it changes any more
   unsigned int timer1CounterValue;
   timer1CounterValue = ICR1;  // see datasheet, page 117 (accessing 16-bit registers)
@@ -32,20 +34,19 @@ ISR (TIMER1_CAPT_vect)
   if (triggered)
     return;
 
-  if (first)
-    {
+  if (first) {
     startTime = (overflowCopy << 16) + timer1CounterValue;
     first = false;
+    TCCR1B =  bit(CS10) | bit(ICES1); // Trigger on rising edge
     return;  
-    }
+  }
     
   finishTime = (overflowCopy << 16) + timer1CounterValue;
   triggered = true;
   TIMSK1 = 0;    // no more interrupts for now
-  }  // end of TIMER1_CAPT_vect
+}  // end of TIMER1_CAPT_vect
   
-void prepareForInterrupts ()
-  {
+void prepareForInterrupts () {
   noInterrupts ();  // protected code
   first = true;
   triggered = false;  // re-arm for next time
@@ -53,25 +54,24 @@ void prepareForInterrupts ()
   TCCR1A = 0;
   TCCR1B = 0;
   
-  TIFR1 = bit (ICF1) | bit (TOV1);  // clear flags so we don't get a bogus interrupt
+  TIFR1 = bit(ICF1) | bit(TOV1);  // clear flags so we don't get a bogus interrupt
   TCNT1 = 0;          // Counter to zero
   overflowCount = 0;  // Therefore no overflows yet
   
   // Timer 1 - counts clock pulses
-  TIMSK1 = bit (TOIE1) | bit (ICIE1);   // interrupt on Timer 1 overflow and input capture
+  TIMSK1 = bit(TOIE1) | bit(ICIE1);   // interrupt on Timer 1 overflow and input capture
   // start Timer 1, no prescaler
-  TCCR1B =  bit (CS10) | bit (ICES1);  // plus Input Capture Edge Select (rising on D8)
-  interrupts ();
-  }  // end of prepareForInterrupts
+  TCCR1B =  bit(CS10);                // plus Input Capture Edge Select (falling on D8)
+  interrupts();
+}  // end of prepareForInterrupts
   
 
-void setup () 
-  {
+void setup ()  {
   Serial.begin(115200);       
   Serial.println("Frequency Counter");
   // set up for interrupts
   prepareForInterrupts ();   
-  } // end of setup
+} // end of setup
 
 void loop () 
   {
